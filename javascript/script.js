0d149d2c485e735c8e59da4219d3d34a2a86686e
giovannimariaciclosi <giovannimariaciclosi@gmail.com>
2023-02-23 15:34:45 +0100

    feat: exercise completed

--- javascript/script.js
+++ javascript/script.js
@@ -27,51 +27,12 @@ Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagi
 - Stampo il prezzo finale con massimo due decimali
 */
 
-// let userTripKm = parseInt(prompt("Inserisci il numero di chilometri che vuoi percorrere"));
-//   // console.log(userTripKm);
-
-// let userAge = parseInt(prompt("Inserisci la tua età"));
-//   // console.log(userAge);
-
-// let ticketPriceFull = (userTripKm * 0.21).toFixed(2);
-//   // console.log(ticketPriceFull);
-
-// let ticketPrice20Discount = (ticketPriceFull - ((ticketPriceFull / 100) * 20)).toFixed(2);
-//   // console.log(ticketPrice20Discount);
-
-// let ticketPrice40Discount = (ticketPriceFull - ((ticketPriceFull / 100) * 40)).toFixed(2);
-//   // console.log(ticketPrice40Discount);
-
-// if (userAge < 18) {
-
-//   // console.log(ticketPrice20Discount);
-//   document.writeln(`
-//   Il prezzo del biglietto intero è di ${ticketPriceFull + " €"}.
-//   Essendo minore di 18 anni ha diritto ad uno sconto del 20%.
-//   Il prezzo finale del biglietto scontato è di ${ticketPrice20Discount + " €"}`)
-
-// } else if (userAge > 65) {
-
-//   // console.log(ticketPrice40Discount);
-//   document.writeln(`
-//   Il prezzo del biglietto intero è di ${ticketPriceFull + " €"}.
-//   Essendo over 65 ha diritto ad uno sconto del 40%.
-//   Il prezzo finale del biglietto scontato è di ${ticketPrice40Discount + " €"}`)
-
-// } else {
-
-//   // console.log(ticketPriceFull);
-//   document.writeln(`
-//   Il prezzo del biglietto intero è di ${ticketPriceFull + " €"}.
-//   Non hai diritto a sconti.. sfigato!`)
-
-// }
-
-
 let userTripKmEl = document.getElementById("userTripKm");
 let userAge = document.getElementById("userAge");
 let userNameSurname = document.getElementById("userNameSurname");
 let userInfoButton = document.getElementById('userInfoBtn');
+let resetUserInfoButton = document.getElementById('resetUserInfoBtn');
+
 
 let pricePerKm = 0.21
 let baseTicketPrice;
@@ -82,27 +43,48 @@ userInfoButton.addEventListener('click', function() {
   // tutto il codice che vogliamo eseguire all'occorrenza dell'evento
   console.log(userTripKmEl.value);
   console.log(userAge.value);
+  console.log(userNameSurname.value);
 
   if (userAge.value < 18) {
     let baseTicketPrice = (userTripKmEl.value * pricePerKm);
-    let finalPrice = baseTicketPrice - baseTicketPrice / 100 * 20;
+    let finalPrice = (baseTicketPrice - baseTicketPrice / 100 * 20).toFixed(2) + "€";
 
     console.log("prezzo scontato under18: " + finalPrice);
+
+    document.getElementById("insertUserName").innerHTML = userNameSurname.value;
+    document.getElementById("insertTicketPrice").innerHTML = finalPrice;
+
 
   } else if (userAge.value > 65) {
     
     let baseTicketPrice = (userTripKmEl.value * pricePerKm);
-    let finalPrice = baseTicketPrice - baseTicketPrice / 100 * 20;
+    let finalPrice = (baseTicketPrice - baseTicketPrice / 100 * 40).toFixed(2) + "€";
 
-    finalPrice = baseTicketPrice - baseTicketPrice * 0.4;
     console.log("prezzo scontato over65: " + finalPrice);
+
+    document.getElementById("insertUserName").innerHTML = userNameSurname.value;
+    document.getElementById("insertTicketPrice").innerHTML = finalPrice;
 
   } else {
 
     let baseTicketPrice = (userTripKmEl.value * pricePerKm);
-    let finalPrice = baseTicketPrice - baseTicketPrice / 100 * 20;
+    let finalPrice = (baseTicketPrice).toFixed(2) + "€";
     
-    finalPrice = baseTicketPrice;
     console.log("prezzo: " + finalPrice);
+
+    document.getElementById("insertUserName").innerHTML = userNameSurname.value;
+    document.getElementById("insertTicketPrice").innerHTML = finalPrice;
+
   }
+});
+
+
+resetUserInfoButton.addEventListener('click', function() {
+  // tutto il codice che vogliamo eseguire all'occorrenza dell'evento
+  document.getElementById("insertUserName").innerHTML = "";
+  document.getElementById("insertTicketPrice").innerHTML = "";
+  document.getElementById("userNameSurname").value = "";
+  document.getElementById("userTripKm").value = "";
+  document.getElementById("userAge").value = "";
+
 });
